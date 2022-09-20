f74b2820ef7a000212120a3138ba3ced44c62ae0
sh92sajnapawar <108973943+sh92sajnapawar@users.noreply.github.com>
2022-09-20 15:34:20 +0530

    Add files via upload

---
+++ shape.js
@@ -0,0 +1,14 @@
+// 
+class Shape {
+  constructor() {
+       this.color = "green";
+  }
+  drawShape() {
+
+  }
+  calculateArea() {
+
+  }
+}
+// export class using module.exports
+module.exports = Shape;
